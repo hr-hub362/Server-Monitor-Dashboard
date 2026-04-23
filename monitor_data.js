9c029ee59ec2a86dc3cc4fce81b2d96c82d031a0
hr-hub362 <hr15019225605@gmail.com>
2026-04-23 17:46:20 +0800

    Add files via upload

---
+++ monitor_data.js
@@ -0,0 +1,12 @@
+window.MONITOR_META = {
+  generatedAt: "2026-04-23 16:54:23 +0800",
+  intervalSeconds: 60,
+  retentionHours: 48,
+  processPattern: "ww/datasource"
+};
+window.MONITOR_RECORDS = [
+  {epoch:1776934402,time:"2026-04-23 16:53:22",cpuUsedPct:0.00,memUsedPct:39.42,memUsedMb:1312.51,memTotalMb:3329.78,diskUsedPct:2.07,diskUsedGb:4.83,diskTotalGb:233.02,procFound:true,procPids:"1252,1254,1282,1668,1872,1885,1897,1915,1943,1959,1980,2026,2057,2183,2204,2228,2235,2248",procRestarted:false,procEvent:"none"},
+  {epoch:1776934462,time:"2026-04-23 16:54:22",cpuUsedPct:31.94,memUsedPct:39.00,memUsedMb:1298.76,memTotalMb:3329.78,diskUsedPct:2.07,diskUsedGb:4.83,diskTotalGb:233.02,procFound:true,procPids:"1252,1254,1282,1668,1872,1885,1897,1915,1943,1959,1980,2026,2057,2183,2204,2228,2235,2248",procRestarted:false,procEvent:"none"},
+];
+window.MONITOR_EVENTS = [
+];
